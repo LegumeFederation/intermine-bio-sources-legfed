7fb3ec09a3e3478dd3d471737b0612f1680f060f
Sam Hokin <shokin@ncgr.org>
2016-07-22 18:31:13 -0600

    New datasource parses a text file and loads ExpressionSource, ExpressionSample and ExpressionValue data.

---
+++ legfed-expression/main/src/org/intermine/bio/dataconversion/ExpressionFileConverter.java
@@ -0,0 +1,186 @@
+package org.intermine.bio.dataconversion;
+
+/*
+ * Copyright (C) 2002-2016 FlyMine, Legume Federation
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  See the LICENSE file for more
+ * information or http://www.gnu.org/copyleft/lesser.html.
+ *
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.apache.tools.ant.BuildException;
+import org.intermine.dataconversion.ItemWriter;
+import org.intermine.metadata.Model;
+import org.intermine.objectstore.ObjectStoreException;
+import org.intermine.util.FormattedTextParser;
+import org.intermine.xml.full.Item;
+
+import org.ncgr.pubmed.PubMedSummary;
+
+/**
+ * DataConverter to create items from tab-delimited expression files, like the one downloaded from PvGEA.
+ *
+ * @author Sam Hokin
+ */
+public class ExpressionFileConverter extends BioFileConverter {
+
+    String taxonId;
+    
+    private static final Logger LOG = Logger.getLogger(ExpressionFileConverter.class);
+
+    /**
+     * Constructor.
+     *
+     * @param writer the ItemWriter used to handle the resultant items
+     * @param model the Model
+     * @throws ObjectStoreException os
+     */
+    public ExpressionFileConverter(ItemWriter writer, Model model) throws ObjectStoreException {
+        super(writer, model);
+    }
+
+    /**
+     * Sets the list of taxonIds that should be imported
+     *
+     * @param taxonIds a space-separated list of taxonIds
+     */
+    public void setOrganisms(String taxonIds) {
+        String[] chunks = taxonIds.split(" ");
+        if (chunks.length!=1) {
+            throw new RuntimeException("Only one taxon ID may be supplied.");
+        } else {
+            taxonId = taxonIds;
+        }
+    }
+
+    /**
+     * Called for each file found by ant.
+     *
+     * {@inheritDoc}
+     */
+    public void process(Reader reader) throws Exception {
+
+        LOG.info("Processing expression file:"+getCurrentFile().getName()+"...");
+        
+        // create and store organism
+        Item organism = createItem("Organism");
+        organism.setAttribute("taxonId", taxonId);
+        store(organism);
+
+
+        // parse the tab-delimited file
+        Iterator<?> tsvIter;
+        try {
+            tsvIter = FormattedTextParser.parseTabDelimitedReader(reader);
+        } catch (Exception ex) {
+            throw new RuntimeException("Cannot parse file:"+getCurrentFile().getName(), ex);
+        }
+
+        // 1. source name, PMID, geo, bioProject, sra
+        String[] sourceLine = (String[]) tsvIter.next();
+        String sourceName = sourceLine[0];
+        String pmid = sourceLine[1];
+        String geo = sourceLine[2];
+        String bioProject = sourceLine[3];
+        String sra = sourceLine[4];
+
+        LOG.info("Loading expression source:"+sourceName);
+        Item source = createItem("ExpressionSource");
+        source.setReference("organism", organism);
+        source.setAttribute("name", sourceName);
+        if (geo.length()>0) source.setAttribute("geo", geo);
+        if (bioProject.length()>0) source.setAttribute("bioProject", bioProject);
+        if (sra.length()>0) source.setAttribute("sra", sra);
+        // create and store the publication if it exists; this requires Internet access
+        if (pmid!=null) {
+            try {
+                int id = Integer.parseInt(pmid);
+                PubMedSummary pms = new PubMedSummary(id);
+                Item publication = createItem("Publication");
+                publication.setAttribute("title", pms.title);
+                if (pms.doi.length()>0) publication.setAttribute("doi", pms.doi);
+                if (pms.issue.length()>0) publication.setAttribute("issue", pms.issue);
+                publication.setAttribute("pubMedId", String.valueOf(pms.id));
+                publication.setAttribute("pages", pms.pages);
+                // parse year, month from PubDate
+                String[] dateBits = pms.pubDate.split(" ");
+                publication.setAttribute("year",dateBits[0]);
+                publication.setAttribute("month",dateBits[1]);
+                publication.setAttribute("volume", pms.volume);
+                publication.setAttribute("journal", pms.fullJournalName);
+                // authors collection
+                boolean firstAuthor = true;
+                for (String author : pms.authorList) {
+                    if (firstAuthor) {
+                        publication.setAttribute("firstAuthor", author);
+                        firstAuthor = false;
+                    }
+                    Item authorItem = createItem("Author");
+                    authorItem.setAttribute("name", author);
+                    store(authorItem);
+                    publication.addToCollection("authors", authorItem);
+                }
+                // store it and add reference to ExpressionSource
+                store(publication);
+                source.setReference("publication", publication);
+            } catch (Exception ex) {
+                throw new RuntimeException("Cannot create publication with PMID="+pmid, ex);
+            }
+        }
+        store(source);
+
+        // 2. expression unit
+        String[] unitLine = (String[]) tsvIter.next();
+        String unit = unitLine[0];
+
+        // 3. number of samples
+        String[] numSamplesLine = (String[]) tsvIter.next();
+        int numSamples = Integer.parseInt(numSamplesLine[0]);
+        
+        // 4. Sample name and description in same order as the expression columns
+        Item[] samples = new Item[numSamples];
+        for (int i=0; i<numSamples; i++) {
+            String[] sampleLine = (String[]) tsvIter.next();
+            samples[i] = createItem("ExpressionSample");
+            samples[i].setAttribute("primaryIdentifier", sampleLine[0]);
+            samples[i].setAttribute("description", sampleLine[1]);
+            samples[i].setReference("source", source);
+            store(samples[i]);
+        }
+
+        // 5. expression data - rely on mRNA merge on primaryIdentifier, organism
+        while (tsvIter.hasNext()) {
+            String[] line = (String[]) tsvIter.next();
+            String mRNAId = line[0];
+            Item mRNA = createItem("MRNA");
+            mRNA.setReference("organism", organism);
+            mRNA.setAttribute("primaryIdentifier", mRNAId);
+            store(mRNA);
+            // load the expression values
+            int offset = 1;
+            for (int i=0; i<numSamples; i++) {
+                Item expressionValue = createItem("ExpressionValue");
+                expressionValue.setAttribute("value", line[i+offset]);
+                expressionValue.setAttribute("unit", unit);
+                expressionValue.setReference("expressionSample", samples[i]);
+                expressionValue.setReference("mRNA", mRNA);
+                store(expressionValue);
+            }
+        }
+
+    }
+
+}
